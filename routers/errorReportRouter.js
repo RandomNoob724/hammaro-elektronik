bd47b32da40733784cdb90ea02a3b059c11f2c66
Emil Persson <emilper1999@gmail.com>
2021-03-03 19:23:05 +0100

    Error reporting form created

--- routers/errorReportRouter.js
+++ routers/errorReportRouter.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) => {
-    response.send('Hello there')
+    response.render('error-report.hbs')
 })
 
 module.exports = router
